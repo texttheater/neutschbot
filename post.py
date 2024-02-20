4184f5df8d5f83364dce74c50e1c7a2d0962cdcb
Kilian Evang <noreply@texttheater.net>
2024-02-20 16:00:09 +0100

    Refactoring: moved text generation to function

--- post.py
+++ post.py
@@ -9,7 +9,7 @@ from mastodon import Mastodon
 import requests
 
 
-if __name__ == '__main__':
+def text_verb():
     r = requests.get('https://neutsch.org/api/verbs.php')
     r.raise_for_status()
     verbs = r.json()
@@ -21,6 +21,11 @@ if __name__ == '__main__':
     part_ii = verb['part_ii']
     text = f'{inf} – {ind_praet} – {konj_ii} – {part_ii}'
     text += f' https://neutsch.org/Starke_Verben/{alpha}'
+    return text
+
+
+if __name__ == '__main__':
+    text = text_verb()
     mastodon = Mastodon(
         access_token = 'token.secret',
         api_base_url = 'https://botsin.space',
